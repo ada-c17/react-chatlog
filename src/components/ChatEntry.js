32b7094db88d95b072a2320864d37ddb3230f08d
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-14 16:57:18 -0500

    Add placeholder text for sample chat entry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">Vladimir</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>why are you arguing with me</p>
+        <p className="entry-time">2018-05-29T22:49:06+00:00</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -17,6 +17,11 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender:
+  body:
+  timeStamp:
+  liked:
 };
 
 export default ChatEntry;
