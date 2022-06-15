e4d47e0bb3df76dbb3610acf929ba9fa1d92e8ef
Juli <juli.terapin@gmail.com>
2022-06-15 16:23:26 -0400

    Updated ChatEntry component to include sender, body and timestamp info. Also updated App to include the ChatEntry.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,8 +10,10 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <h1>Chat Entry</h1>
+        <ChatEntry />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
