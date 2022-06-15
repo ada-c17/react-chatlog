2aaee31e7246fb9509ced843afa2e242332aebb2
Lin Liu <lin_6066@yahoo.com>
2022-06-15 13:42:05 -0700

    display chat message via ChatEntry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,12 +3,20 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  const sender = props.sender;
+  let className;
+  if (sender === 'Estragon') {
+    className = 'chat-entry local';
+  } else {
+    className = 'chat-entry remote';
+  }
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    <div className={className}>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">{props.timeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +24,9 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string,
+  timeStamp: PropTypes.instanceOf(Date),
 };
 
 export default ChatEntry;
