7d7c62ba4c5486902a926e16e4607d9b0062ca73
Grace <gpark439@gmail.com>
2022-06-16 01:37:03 -0700

    Test first chat message to show sender name as ChatEntry component

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>Replace with body of ChatEntry</p>
         <p className="entry-time">Replace with TimeStamp component</p>
