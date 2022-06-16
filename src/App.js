d8b17b9f16f15b593c19ebf94eaaa32c4a330614
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-06-15 19:58:58 -0500

    Updated ChatEntry component to use props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const singleMessage = chatMessages[0];
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Log</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={singleMessage.sender}
+          body={singleMessage.body}
+          timeStamp={singleMessage.timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
