257b1847addb3d0052ffdb779c53b5ad2b4ad877
Sabrina Huwang <sabrinahuwang1@gmail.com>
2022-06-15 23:03:11 -0700

    Wave one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chatlog</h1>
       </header>
       <main>
+        <ChatEntry
+          sender="sender"
+          body="body"
+          timeStamp="2018-05-18T22:12:03Z"
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
