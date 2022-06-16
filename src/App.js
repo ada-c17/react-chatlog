20512126f0728b1f5d62be57fc8e95c3f0d51a71
Yonese James <yoneseguerrier@gmail.com>
2022-06-16 18:21:12 -0400

    Initial Commit - Completed Wave 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+
+const chatMessage = chatMessages[0];
 
 const App = () => {
   return (
@@ -9,6 +12,11 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={chatMessage.sender}
+          body={chatMessage.body}
+          timeStamp={chatMessage.timeStamp}>
+        </ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
