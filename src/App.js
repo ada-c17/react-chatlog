188eb0deeb4ec33ceb8ec0487847c99bdab674e2
Jande Royalty <janderoyalty@gmail.com>
2022-06-16 17:57:21 -0500

    wave01 completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  console.log(chatMessages);
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <ChatEntry
+          sender={chatMessages[0].sender}
+          body={chatMessages[0].body}
+          timeStamp={chatMessages[0].timeStamp}
+        ></ChatEntry>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
