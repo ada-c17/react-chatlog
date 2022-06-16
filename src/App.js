9945b5cbe61c1019a59b9a2ca623bb3dada75dd8
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-06-16 15:21:01 -0700

    Completed Wave 1 and passed tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
+const chatMessage = chatMessages[0];
+console.log(chatMessages[0]);
+
 const App = () => {
+  // console.log(chatMessages);
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon </h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatMessage.sender}
+          body={chatMessage.body}
+          timeStamp={chatMessage.timeStamp}
+        ></ChatEntry>
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
