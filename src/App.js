97fd4cb48397b3292e6d5bb9085335e3f3cb7ddb
Uyen Vong <uvong92@gmail.com>
2022-06-15 19:23:15 -0700

    ChatEntry component displays single chat message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import TimeStamp from './components/TimeStamp';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -11,6 +13,13 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender="Vladimir"
+          body="why are you arguing with me"
+          timestamp="2018-05-29T22:49:06+00:00"
+          liked="false"
+        />
+        <TimeStamp />
       </main>
     </div>
   );
