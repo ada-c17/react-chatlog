57be95d2c183c8980cc3d144f99775c0ca8d4e9c
Danielle <dwhyte1226@gmail.com>
2022-06-16 17:25:21 -0500

    updated to render one chat entry component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
@@ -11,6 +12,13 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        {<div>
+          { <ChatEntry
+            sender={ chatMessages[0].sender }
+            body={ chatMessages[0].body }
+            timeStamp= {chatMessages[0].timeStamp}
+        /> }
+        </div>}
       </main>
     </div>
   );
