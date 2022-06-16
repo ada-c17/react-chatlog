0c54e9c65ab5135d8c22132a52a3983eeeff3691
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-15 19:31:32 -0500

    Integrate TimeStamp component into ChatEntry component, pass wave 01 tests

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ChatEntry.css';
+import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
@@ -8,7 +9,7 @@ const ChatEntry = (props) => {
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
-        <p className="entry-time">{props.timeStamp}</p>
+        <TimeStamp time={props.timeStamp} />
         <button className="like">🤍</button>
       </section>
     </div>
