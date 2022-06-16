b872465bcdbe16e49b164781c1806c1de689fb5f
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-15 20:01:19 -0500

    Integrate ChatLog into App; incorporate messages.json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+// import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatEntry />
+        <ChatLog entries={chatMessages} />
       </main>
     </div>
   );
