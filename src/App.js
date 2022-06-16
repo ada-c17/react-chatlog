9bfc80f0bd0f38e270cbcdde4525b159383a8325
tinghj17 <tinghj17@gmail.com>
2022-06-15 20:59:56 -0700

    Chat entry tests passed wave01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
+
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>
+          Chat between {chatMessages[0].sender} and {chatMessages[1].sender}
+        </h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry chatMessages={chatMessages} />
       </main>
     </div>
   );
