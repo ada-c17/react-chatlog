232a06cc48e5449692a13568c0e95a6e2be77320
Tyrah <tyrah96@gmail.com>
2022-06-16 13:57:41 -0700

    practice rendering one object from messages array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+// import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry.js';
+
+const chatData = [
+  {
+    id: 1,
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00',
+  },
+];
 
 const App = () => {
   return (
@@ -9,6 +19,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry chatData={chatData} />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+// import Chat from './Chat';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ chatData }) => {
+  let chat = chatData[0];
+  let sender = chat.sender;
+  let body = chat.body;
+  let timeStamp = chat.timeStamp;
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
+
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{body}</p>
+
+        <p className="entry-time">{timeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +23,14 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  chatData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      sender: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      timeStamp: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default ChatEntry;
