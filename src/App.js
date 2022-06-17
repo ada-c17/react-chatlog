9684e28fa299c28c8c5cff379d66bea23dd307ce
Vida Maleki <vida.basmani@gmail.com>
2022-06-16 22:41:45 -0700

    wave one and two is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry'
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
+      <ChatEntry
+          body={chatMessages[0].body}
+          sender={chatMessages[0].sender}
+          timeStamp={chatMessages[0].timeStamp}
+          /> 
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
