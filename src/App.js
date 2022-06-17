081a8538bbac90e13b9fb35318ed1d0ca70a72e2
Gabriela Webb <gabw813@gmail.com>
2022-06-17 02:44:19 -0700

    wave01 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import TimeStamp from './components/TimeStamp';
+
+// const chatMessages =
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={chatMessages[0]['sender']}
+          body={chatMessages[0]['body']}
+          timeStamp={chatMessages[0]['timeStamp']}
+        ></ChatEntry>
+
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        {/* <TimeStamp></TimeStamp> */}
       </main>
     </div>
   );
