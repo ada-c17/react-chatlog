e2b8a64c296d30355438fd10b8709391d60d9665
Liqingcathy <cathy.glc@gmail.com>
2022-06-16 20:44:42 -0700

    like eventhandler and left right arrange

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+// import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,19 +2,29 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
-
+import { useState } from 'react';
 const ChatEntry = (props) => {
   const sender = props.sender;
   const body = props.body;
   const timeStamp = props.timeStamp;
+ 
+
+  const [liked, setFillHeart] = useState(props.liked);
+  let like = '🤍';
+  const fillRedHeart = () => {
+    setFillHeart(!liked);
+  }
   
+  like = !liked ? '🤍' : '❤️';
+  let leftRight = (sender === 'Vladimir') ? 'local' : 'remote';
+   
   return (
-    <div className="chat-entry local">
+    <div className={`chat-entry ${leftRight}`}>
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
         <p className="entry-time"><TimeStamp time={timeStamp} /></p>
-        <button className="like">🤍 </button>
+        <button onClick={fillRedHeart}>{like}</button>
       </section>
     </div>
   );
