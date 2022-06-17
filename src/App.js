cc916799351613ca8d09c06f6dfb9a94079321e2
mit <mit.magh@gmail.com>
2022-06-17 09:42:46 -0700

    End of wave1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,6 +10,11 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          sender="Mitra"
+          body="I dont know"
+          timeStamp="2018-05-29T22:49:06+00:00"
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
