8136c19e12f504fe08c260877e6459d0d0ae3f57
Nishat <nsalsabil25@gmail.com>
2022-06-17 03:15:52 -0700

    added h1 title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
