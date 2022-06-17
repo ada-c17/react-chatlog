a6bbe317e88eeb59475be61d6a9c77108bb1763d
Danielle <dmbecker24@gmail.com>
2022-06-17 12:03:49 -0700

    Add a single ChatEntry component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,6 +10,11 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={chatMessages[1]['sender']}
+          body={chatMessages[1]['body']}
+          timeStamp={chatMessages[1]['timeStamp']}
+        ></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ sender, body, timeStamp }) => {
+  let timeStampDate = new Date(timeStamp);
+  let currentDate = new Date();
+  let timeStampYear = timeStampDate.getFullYear();
+  let currentYear = currentDate.getFullYear();
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{body}</p>
+        <p className="entry-time">{currentYear - timeStampYear} years ago</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +21,9 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
 };
 
 export default ChatEntry;
