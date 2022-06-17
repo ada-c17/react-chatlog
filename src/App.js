1576f23a1d52a88092eb88f1e26c8c593a9d1acb
Natalia <natalia.woodson@gmail.com>
2022-06-16 22:50:03 -0400

    completed wave 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const chatData = {
+    id: 1,
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00',
+    liked: false,
+  };
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={chatData.sender}
+          body={chatData.body}
+          timeStamp={chatData.timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const ChatEntry = (props) => {
+  const timeAgo = moment(props.timeStamp).fromNow();
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">{timeAgo}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +18,11 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number,
+  sender: PropTypes.string,
+  body: PropTypes.string,
+  timestamp: PropTypes.string,
+  liked: PropTypes.bool,
 };
 
 export default ChatEntry;
