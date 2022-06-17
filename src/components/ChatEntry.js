41efec2353a799c826a0c7caa9c821a13b2bcca6
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-16 21:43:34 -0700

    Wave 1 functionality

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
-import '../data/messages.json';
+import TimeStamp from './TimeStamp';
 
 
 const ChatEntry = (props) => {
+  
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
-        <p className="entry-time">{props.timeStamp}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -19,9 +20,11 @@ const ChatEntry = (props) => {
 
 ChatEntry.propType = {
   //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
