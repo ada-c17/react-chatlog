139d46de99bf6de30cbe9213f62f8fdfd41bb720
Nikki Torab <nikkitorab@gmail.com>
2022-06-17 12:02:45 -0700

    wave 1 finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const chatData = chatMessages;
+
   return (
     <div id="App">
       <header>
@@ -11,6 +14,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatData[0].sender}
+          body={chatData[0].body}
+          timeStamp={chatData[0].timeStamp}
+        />
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const timeComponent = <TimeStamp time={props.timeStamp} />;
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      {/* <h2 className="entry-name">Replace with name of sender</h2> */}
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        {/* <p>Replace with body of ChatEntry</p> */}
+        <p>{props.body}</p>
+        {/* <p className="entry-time">Replace with TimeStamp component</p> */}
+        <p className="entry-time">{timeComponent}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -17,6 +23,11 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  // id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  // liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
