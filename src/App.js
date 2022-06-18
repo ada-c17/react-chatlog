144ed488884b5cc9f236070aa588997d8ebdccf6
shelby <faulconershelby@gmail.com>
2022-06-17 17:16:58 -0700

    completed the first wave testing by writing the ChatEntry component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry'
 
 const App = () => {
+
+  const chatMessageData = chatMessages[0];
+  console.log(chatMessages);
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry sender= {chatMessageData.sender} 
+          body = {chatMessageData.body} 
+          timeStamp={chatMessageData.timeStamp}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
+
+
+// const timeDiff = (props) => {
+//   let currentTime = new Date();
+//   let time = currentTime.getUTCDate();
+//   let newTime = time - props.timeStamp
+//   return newTime
+// };
 
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
         <button className="like">🤍</button>
       </section>
     </div>
   );
 };
 
+
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
 };
 
 export default ChatEntry;
