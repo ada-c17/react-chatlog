36cbedef7d1429ffba1c68c38c341ba351f1362d
Alexa Coffman <coffman.alexa@gmail.com>
2022-06-18 13:47:26 -0700

    Added updateLikes helper and display counter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
+let totalLikes = 0;
+
+const updateLikes = (entryData) => {
+  let likes = 0;
+  for (const entry of entryData) {
+    if (entry.liked) {
+      likes += 1;
+    }
+  }
+  totalLikes = likes;
+};
+
 const App = () => {
   const [entryData, setEntryData] = useState(chatMessages);
-
   const updateEntryData = (updatedEntry) => {
     const entries = entryData.map((chatEntry) => {
       if (chatEntry.id === updatedEntry.id) {
@@ -15,12 +26,18 @@ const App = () => {
       }
     });
     setEntryData(entries);
+    updateLikes(entries);
   };
 
   return (
     <div id="App">
       <header>
-        <h1>Chat Log</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
+        <section className="header section">
+          <h4 id="heartWidget" className="widget">
+            {totalLikes} ❤️s
+          </h4>
+        </section>
       </header>
       <main>
         <ChatLog entries={entryData} onUpdateEntry={updateEntryData}></ChatLog>
