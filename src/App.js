1273c161eeff7d6b6052fa9c1ab84afe377a93a7
Morgan Buntin <mmbuntin@gmail.com>
2022-06-19 15:09:24 -0400

    passing wave 3 tests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const [entriesData, setEntriesData] = useState(chatMessages);
+
+  const calcLikes = (entriesData) => {
+    let likeCount = 0;
+
+    for (const entry of entriesData) {
+      if (entry.liked) {
+        likeCount++;
+      }
+    }
+    return likeCount;
+  };
+
+  const updateEntriesData = (updatedEntry) => {
+    const entriesList = entriesData.map((entry) => {
+      if (entry.id === updatedEntry.id) {
+        return updatedEntry;
+      } else {
+        return entry;
+      }
+    });
+    setEntriesData(entriesList);
+  };
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>{calcLikes(entriesData)} ❤️s</h1>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={entriesData} onUpdateEntry={updateEntriesData} />
       </main>
     </div>
   );
