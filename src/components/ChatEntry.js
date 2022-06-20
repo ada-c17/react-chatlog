d8357c8aa9d2e67e71b47bfe4d84ef6f849a269e
Jande Royalty <janderoyalty@gmail.com>
2022-06-20 17:57:09 -0500

    Wave03: hearts completed

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ id, sender, body, liked, timeStamp, countLikes }) => {
+  // const [likesCount, setLikesCount] = useState(0);
+  const [likeHeart, setLikeHeart] = useState('🤍');
+
+  const onClickLike = () => {
+    // if(likeHeart === '🤍' ) {
+    //   setLikeHeart('❤️');
+    // }
+    const heartColor = likeHeart === '🤍' ? '❤️' : '🤍';
+    setLikeHeart(heartColor);
+  };
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">{props.sender}</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>{props.body}</p>
+        <p>{body}</p>
         <p className="entry-time">
-          <TimeStamp time={props.timeStamp}></TimeStamp>
+          <TimeStamp time={timeStamp}></TimeStamp>
         </p>
-        <button className="like">🤍</button>
+        <button onClick={onClickLike} className="like">
+          {likeHeart}
+        </button>
       </section>
     </div>
   );
