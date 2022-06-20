156e90d5b92d0064d470851caba3f30cf710374f
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-06-20 12:24:06 -0700

    running like count that upgrades with clicking liked button on chat entry, all wave 03 tests passing

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import chatMessages from './data/messages.json';
 const App = () => {
   const [entries, setEntries] = useState(chatMessages);
 
+  const [likeCount, setLikes] = useState(0);
+
   const handleLikes = (id) => {
     const newEntries = [];
     for (const entry of entries) {
       if (entry.id === id) {
         entry.liked = !entry.liked;
+        if (entry.liked === true) {
+          setLikes(likeCount + 1);
+        } else if (entry.liked === false) {
+          setLikes(likeCount - 1);
+        }
       }
       newEntries.push(entry);
     }
@@ -20,7 +27,8 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Log</h1>
+        <h2>{likeCount} ❤️s</h2>
       </header>
       <main>
         <ChatLog entries={entries} likedCallback={handleLikes} />
