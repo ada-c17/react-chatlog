7d70a0615bf0a4e66ce0c31ed1764ac9847e2591
Huma Hameed <mrskq1@gmail.com>
2022-06-19 23:30:19 -0500

    wave1:complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,8 +10,13 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <ChatEntry
+          sender={chatMessages[0].sender}
+          body={chatMessages[0].body}
+          timeStamp={chatMessages[0].timeStamp}
+        ></ChatEntry>
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
