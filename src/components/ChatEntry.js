5c781f9671d0955b7998fa957d8a7e51b951cbe2
GitHub4Gigi <g_maycock@outlook.com>
2022-06-19 20:50:46 -0700

    Waves 1 - 3 complete

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+
+const ChatEntry = ({id, sender, body, timeStamp, liked, toggleLike}) => {
+  const handleToggleLiked = () => {
+    const updatedEntry = {
+      id: id, sender: sender, body: body, timeStamp: timeStamp, liked: !liked
+    };
+    toggleLike(updatedEntry)
+
+  }
+  const heartColor = liked ? '❤️' : '🤍';
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{body}</p>
+        <p className="entry-time"><TimeStamp time={timeStamp}/></p>
+        <button className="like" onClick={handleToggleLiked}>{heartColor}</button>
       </section>
     </div>
-  );
-};
+  )};
+
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
