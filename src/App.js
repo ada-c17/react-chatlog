c816dbbc122d6de719147392070746bcdc0df71c
Danielle <dwhyte1226@gmail.com>
2022-06-20 02:34:20 -0500

    added functions to update messages and count likes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const [chatMessageData, setChatData] = useState(chatMessages);
+
+  const updateChatData = (updatedMessage) => {
+    const messages = chatMessageData.map((message) => {
+      if (message.id === updatedMessage.id) {
+        return updatedMessage;
+      } else {
+        return message;
+      }
+
+    });
+    setChatData(messages);
+  };
+
+
+  const countLikes = () => {
+    return (chatMessageData.reduce(
+      (previousValue, currentValue) => {
+        const val = currentValue.liked ? 1 : 0;
+        return previousValue + val;
+      }, 0));
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <h2> {countLikes()} ❤️s </h2>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
         {<div>
           { <ChatLog
-            entries={ chatMessages }
+            entries={ chatMessageData }
+            onUpdateMessage={ updateChatData }
         /> }
         </div>}
       </main>
