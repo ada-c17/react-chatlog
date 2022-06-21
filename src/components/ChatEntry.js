0e993f32596e8aab11a3a1c2a3b1240f1691546b
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-21 12:46:42 -0500

    Add id and liked props to chat entry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatEntry.css';
 import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
 
 const ChatEntry = (props) => {
+  // set class name for message based on sender
   const entryClassName =
     props.sender === 'Vladimir' ? 'chat-entry local' : 'chat-entry remote';
+
+  const [likeButton, setLikeButton] = useState('🤍');
+
+  const toggleLikeButton = () => {
+    console.log('You clicked the like button.');
+  };
 
   return (
     <div className={entryClassName}>
@@ -13,17 +20,21 @@ const ChatEntry = (props) => {
       <section className="entry-bubble">
         <p>{props.body}</p>
         <TimeStamp time={props.timeStamp} />
-        <button className="like">🤍</button>
+        <button className="like" onClick={toggleLikeButton}>
+          🤍
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   // timeStamp proptype will need to be updated to require date, not just string
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
