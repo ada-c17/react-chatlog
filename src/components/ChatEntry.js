9d0c25d209c31fce437b337877aecbd2f75b01b1
Hillary Smith <hillaryhsmith@gmail.com>
2022-06-21 14:36:28 -0700

    added state and toggle for heart icon

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
+import { useState } from 'react';
 
 const ChatEntry = (props) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLiked = () => {
+    setIsLiked(!isLiked);
+  };
+  
+  const heartIcon = isLiked ? '❤️' : '🤍';
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
@@ -12,7 +21,7 @@ const ChatEntry = (props) => {
         <p className="entry-time">
           <TimeStamp time={props.timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={toggleLiked}>{heartIcon}</button>
       </section>
     </div>
   );
