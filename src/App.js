a99fc546f93162012aa622689610e138949a378b
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-06-20 18:10:52 -0700

    Wave 1 and 2 completed!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+import messages from './data/messages.json';
+import ChatLog from './components/ChatLog';
+import { useState } from 'react';
 
 const App = () => {
+  const [entries, setEntries] = useState(messages);
+  let [heartsCount, setHeartsCount] = useState(0);
+
+  const flipHearts = (id) => {
+    console.log('function called')
+    const newEntries = [];
+    for (const en of entries){
+      let entry = JSON.parse(JSON.stringify(en)); // deep copy?
+
+      if (entry.id === id){
+        entry.liked = !entry.liked;
+        if (entry.liked){
+          heartsCount++;
+        } else {
+          heartsCount--;
+        }
+      }
+      newEntries.push(entry);
+    }
+    setEntries(newEntries)
+    setHeartsCount(heartsCount)
+    console.log(heartsCount)
+  };
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
+        <section>{heartsCount}{(heartsCount>1 || heartsCount===0) ? ' ❤️s': ' ❤️'}</section>
       </header>
       <main>
+        <ChatLog 
+          entries={messages}
+          heartsCallback={flipHearts}>
+        </ChatLog>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
