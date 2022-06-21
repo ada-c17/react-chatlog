07d7731bc090d4f2abead7a731f84444d7755b32
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-21 13:05:11 -0500

    Add state and toggleLikeButton to modify message data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 // import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const [chatData, setChatData] = useState(chatMessages);
+
+  const toggleLikeButton = (id) => {
+    const messages = chatData.map((message) => {
+      if (message.id === id) {
+        message.liked = !message.liked;
+      }
+
+      return message;
+    });
+
+    setChatData(messages);
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
+        <ChatLog entries={chatData} onUpdateLikeStatus={toggleLikeButton} />
       </main>
     </div>
   );
