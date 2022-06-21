8279e7b12f590f9da67805fd9a638d4f5da60ceb
Esther Annorzie <estherannorzie@gmail.com>
2022-06-21 13:56:30 -0400

    Props attribute changed to sender

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -6,7 +6,7 @@ import TimeStamp from './TimeStamp';
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">{props.name}</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>
           <>{props.body}</>
