4359548ebd74a5c1b3f6ea37f48bfde7fd687d38
Uyen Vong <uvong92@gmail.com>
2022-06-21 21:08:27 -0700

    adds click event to toggle liked value and displays total likes

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,8 +4,11 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const onLikeButtonClick = () => {
+    props.updateEntries(props.id);
+  };
+
   return (
-    // <div className="chat-entry local">
     <div
       className={
         props.sender === 'Vladimir' ? 'chat-entry local' : 'chat-entry remote'
@@ -17,7 +20,9 @@ const ChatEntry = (props) => {
         <p className="entry-time">
           <TimeStamp time={props.timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        <button onClick={onLikeButtonClick} className="like">
+          {props.liked ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
@@ -26,8 +31,8 @@ const ChatEntry = (props) => {
 ChatEntry.propTypes = {
   //Fill with correct proptypes
   sender: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired,
-  liked: PropTypes.bool.isRequired,
+  body: PropTypes.string,
+  liked: PropTypes.bool,
 };
 
 export default ChatEntry;
