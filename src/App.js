c8dbb68cd160348e294a289539c169f08ab9b256
Kassidy Buslach <kjbuslach@gmail.com>
2022-06-21 17:56:30 -0700

    unsaved work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
   console.log(chatMessages);
+  const [liked, setLiked] = useState(0);
+  const countLikes = () => {};
   return (
     <div id="App">
       <header>
         <h1>Chatty chats</h1>
+        <h3> {countLikes} </h3>
       </header>
       <main>
         <ChatLog entries={chatMessages}></ChatLog>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
