7395045c73d0e023d819cbbee1d9c868abc35447
angelica <djyl.ahn@gmail.com>
2022-06-22 11:16:53 -0500

    passes up to wave 02

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const buttonEmoji = props.liked ? '❤️' : '🤍';
+  const changeLiked = () => {
+    const updatedMessage = {
+      id: props.id,
+      body: props.body,
+      sender: props.sender,
+      timeStamp: props.timeStamp,
+      liked: !props.liked,
+    };
+    props.onUpdate(updatedMessage);
+  };
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{props.body}</p>
+        <p className="entry-time">
+          <TimeStamp time={props.timeStamp} />
+        </p>
+        <button className="like" onClick={changeLiked}>
+          {buttonEmoji}
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  body: PropTypes.string.isRequired,
+  sender: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
