cfb7351f36d5ad9acbb79d19ce7a5648caadb9d3
sana <sanakakadan22@yahoo.com>
2022-06-22 18:06:54 +0200

    add all waves passing all tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  // console.log(chatMessages);
+
+  // const [likedMap, setLikedMap] = useState({})
+  const [likedCount, setLikedCount] = useState(0);
+
+  const addLike = () => setLikedCount(likedCount + 1);
+  const removeLike = () => setLikedCount(likedCount - 1);
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
+        <section id="heartWidget">{likedCount} ❤️s</section>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog
+          addLike={addLike}
+          removeLike={removeLike}
+          entries={chatMessages}
+        ></ChatLog>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({ sender, body, timeStamp, liked, addLike, removeLike }) => {
+  const [like, setLike] = useState(liked);
+
+  const handleOnClick = () => {
+    if (like) {
+      removeLike();
+    } else {
+      addLike();
+    }
+
+    setLike(!like);
+  };
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{body}</p>
+        <p className="entry-time">
+          <TimeStamp time={timeStamp} />
+        </p>
+        <button className="like" onClick={handleOnClick}>
+          {like ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool,
+  addLike: PropTypes.func,
+  removeLike: PropTypes.func,
 };
 
 export default ChatEntry;
