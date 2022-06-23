d97d8bdcd186889880534c19bc9be79b188dc3a6
Ying Ye <yingwilliams1406@outlook.com>
2022-06-22 22:10:57 -0400

    finished wave1 and wave2, still working on wave3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import './App.css';
+// import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const [messages, setMessages] = React.useState([]);
+  //
+  React.useEffect(() => {
+    // populate message state with stored messages
+    setMessages(chatMessages);
+  }, []);
+
+  const heartToggling = (updatedMessage) => {
+    const updatedMessages = messages.map((message) => {
+      if (message.id === updatedMessage.id) {
+        return updatedMessage;
+      } else {
+        return message;
+      }
+    });
+
+    setMessages(updatedMessages);
+  };
+
+  //counting red hearts~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  const countingLikes = (messages) => {
+    let heartCount = 0;
+    for (const eachMessage of messages) {
+      if (eachMessage.liked) {
+        heartCount += 1;
+      }
+    }
+    return heartCount;
+  };
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // remove it later !!!!!!!!!!!!!!!!!!!!!
+  // console.log(chatEntryComp);
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>
+          Chat between {chatMessages[0]['sender']} and{' '}
+          {chatMessages[1]['sender']}
+        </h1>
+        <h2>{countingLikes}❤️s</h2>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        // Wave 02: Render ChatLog component */}
+        <ChatLog entries={messages} heartToggling={heartToggling}></ChatLog>
       </main>
     </div>
   );
 };
 
 export default App;
+
+//create count state in the app comp, pass down the updated message to the chatEntry comp,
+//every time a heart is clicked, increase the count by 1
