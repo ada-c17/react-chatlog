b9dbd3e3e62659e280d2384c463e61de99806e80
San <san.robinxin@gmail.com>
2022-06-23 20:37:46 +0900

    wave 3

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const onLikeButtonClick = () => {
+    const updatedMessage = {
+      id: props.id,
+      sender: props.sender,
+      body: props.body,
+      timeStamp: props.timeStamp,
+      liked: !props.liked,
+    };
+    props.onUpdate(updatedMessage);
+  }
+
+  const likeButton = props.liked ? '💚' : '🤍';
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
-      <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+    <div className="chat-entry local" key={props.id}>
+      <h2 className="entry-name"> { props.sender } </h2>
+      <section className="chat-entry.remote entry-bubble" >
+        <p> { props.body } </p>
+        <p className="entry-time"> <TimeStamp time={props.timeStamp} />  
+        </p>
+        <button className="like" onClick={onLikeButtonClick}> 
+        {likeButton} 
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number,
+  sender: PropTypes.string,
+  body: PropTypes.string,
+  timeStamp: PropTypes.string,
+  liked: PropTypes.bool,
+  onUpdate: PropTypes.func,
 };
 
 export default ChatEntry;
