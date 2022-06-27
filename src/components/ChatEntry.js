d42ffd46058670df06463e77b916df5a38a7d59e
camilla <pasadenastringtheory@gmail.com>
2022-06-26 18:17:29 -0700

    Modifies all files, implements components

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,21 +2,68 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
+// {
+//   "id": 1,
+//   "sender":"Vladimir",
+//   "body":"why are you arguing with me",
+//   "timeStamp":"2018-05-29T22:49:06+00:00",
+//   "liked": false
+// }
+
+
+function getDifferenceInYears(timeStamp) {
+  // key={timeStamp}
+  const today = new Date();
+  const year = today.getFullYear();
+
+  const pastDay = new Date(timeStamp);
+  const pastYear = pastDay.getFullYear();
+
+  const difference = year - pastYear;
+  return difference;
+}
+
 const ChatEntry = (props) => {
+  // console.log(props)
+  const timeInYears = getDifferenceInYears(props.timeStamp) + ' years ago';
+
+  const onLikeButtonClick = () => {
+    const updatedMessage = {
+        id: props.id,
+        sender: props.sender,
+        body: props.body,
+        timeStamp: props.timeStamp,
+        liked: !props.liked
+    };
+
+    // Invoke the function passed in through the prop named "onUpdate"
+    // This function is referenced by the name "updateStudentData" in App
+    props.onUpdateChatData(updatedMessage);
+  };
+
+  // className stuff here
+  // const likeButton = props.liked ? 'presentblue' : 'absentviolet';
+
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    // <div className="chat-entry local">
+    <div className={props.sender==='Vladimir'?'chat-entry local':'chat-entry remote'}>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
-        <button className="like">🤍</button>
+        <p>{props.body}</p>
+        <p className="entry-time">{timeInYears}</p>
+        <button onClick={onLikeButtonClick} className="like">{!props['liked']?'🤍':'💙'}</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id: PropTypes.number,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool,
+  onUpdateChatData: PropTypes.func
 };
 
 export default ChatEntry;
